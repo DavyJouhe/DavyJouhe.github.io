06db00776d6a3d4d751bfdb9ea4c412557f294cf
DavyJouhe <1716981973@qq.com>
2018-08-13 16:33:12 +0800

    fix the code

---
+++ js/index.js
@@ -0,0 +1,22 @@
+/* 
+* @Author: Jouhe
+* @Date:   2018-08-13 14:38:25
+* @Last Modified time: 2018-08-13 15:00:22
+*/
+
+$(document).ready(function(){
+    // filter items on click
+    $('#filters').on( 'click', '.type', function() {
+        var aurl = $(this).attr("date-src");
+        $("#myiframe").attr("src",aurl);
+    });
+    
+    // change is-checked class on buttons
+    $('.filters').each( function( i, typeGroup ) {
+        var $typeGroup = $( typeGroup );
+        $typeGroup.on( 'click', '.type', function() {
+            $typeGroup.find('.active').removeClass('active');
+            $( this ).addClass('active');
+        });
+    });
+});
